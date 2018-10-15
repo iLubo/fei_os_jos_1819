48eee7859aecbc9f80d28a0c104572873eec65b9
iLubo <lubo.papcun@gmail.com>
2018-10-15 19:31:37 +0200

    cvicenie 5 - uloha 1.1

--- kern/pmap.c
+++ kern/pmap.c
@@ -384,7 +384,31 @@ pte_t *
 pgdir_walk(pde_t *pgdir, const void *va, int create)
 {
 	// Fill this function in
-	return NULL;
+	size_t pg_dir_index = PDX(va);
+ 	pde_t *pg_dir_entry = (pgdir + pg_dir_index);
+ 	pte_t *pg_table;
+
+ 	if(!(*pg_dir_entry & PTE_P)) {
+ 		if(!create)
+ 			return NULL;
+
+ 		struct PageInfo *new_page_entry = page_alloc(ALLOC_ZERO);
+
+ 		if(!new_page_entry)
+ 			return NULL;
+
+ 		*pg_dir_entry = page2pa(new_page_entry) | PTE_P | PTE_W | PTE_U;
+ 		new_page_entry->pp_ref++;
+ 		pg_table = (pte_t *) KADDR(page2pa((struct PageInfo *) new_page_entry));
+ 	}
+ 	else
+ 	{
+ 		pg_table = KADDR(PTE_ADDR(*pg_dir_entry));
+ 	}
+
+ 	return &pg_table[PTX(va)];
+	
+	//return NULL;
 }
 
 //
